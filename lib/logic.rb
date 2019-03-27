1dccce97163b8b51a2163da1d7d6a665468f6832
Etienne Mustow <etienne.o.mustow@gmail.com>
2019-03-27 11:54:30 +0000

    logic can calculate any of the variables given three other variable values have been given

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,10 +1,22 @@
 class Logic
 
   attr_accessor :interest_paid, :interest_rate, :amount_borrowed, :years
-  def initialize(interest_paid: nil, interest_rate: nil, amount_borrowed: nil, years: nil)
+  def initialize(interest_paid = nil, interest_rate = nil, amount_borrowed = nil, years = nil)
     @interest_paid = interest_paid
     @interest_rate = interest_rate
     @amount_borrowed = amount_borrowed
     @years = years
+  end
+
+  def calculate
+    if @interest_paid == nil
+      @interest_paid = @interest_rate.to_f * @amount_borrowed.to_f * @years
+    elsif @interest_rate == nil
+      @interest_rate = @interest_paid.to_f / (@amount_borrowed.to_f * @years)
+    elsif @amount_borrowed == nil
+      @amount_borrowed = @interest_paid.to_f / (@interest_rate.to_f * @years)
+    elsif @years == nil
+      @years = @interest_paid.to_f / (@interest_rate.to_f * @amount_borrowed.to_f)
+    end
   end
 end
--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -5,4 +5,24 @@ describe Logic do
     logic = Logic.new
     expect(logic).to be_truthy
   end
+  it "can initialize with only three of the four variables given" do
+    logic = Logic.new(nil, 0.01, 2000, 10)
+    logic1 = Logic.new(200, nil, 2000, 10)
+    expect(logic).to be_truthy
+    expect(logic1).to be_truthy
+  end
+  it "can calculate the missing variable based on the other three variables given" do
+      logic = Logic.new(nil, 0.01, 2000, 10)
+      logic.calculate
+      logic1 = Logic.new(200, nil, 2000, 10)
+      logic1.calculate
+      logic2 = Logic.new(200, 0.01, nil, 10)
+      logic2.calculate
+      logic3 = Logic.new(200, 0.01, 2000, nil)
+      logic3.calculate
+      expect(logic.interest_paid).to eq(200)
+      expect(logic1.interest_rate).to eq(0.01)
+      expect(logic2.amount_borrowed).to eq(2000)
+      expect(logic3.years).to eq(10)
+  end
 end
